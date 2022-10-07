4911bbd789596146e0807d035446bc58e8bee723
Viral-Gajera <viral.gajera218@gmail.com>
2022-10-08 00:48:18 +0530

    All bug fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -313,3 +313,18 @@
     });
   });
 })();
+
+
+// selecting elements
+let list = document.querySelectorAll('.nav-link');
+
+for (let i=0; i<list.length; i++){
+  list[i].addEventListener('click', function(){
+    for(let j=0; j<list.length; j++){
+      list[j].classList.remove('active');
+    }
+    list[i].classList.add('active');
+  })
+}
+
+console.log(list);
